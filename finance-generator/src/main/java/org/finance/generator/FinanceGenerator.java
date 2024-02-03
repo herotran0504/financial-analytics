82256c7dcecb4ba3b3ab36ad1a08ad71011b4c8f
herotran0504 <herotran0504@gmail.com>
2024-02-03 17:01:36 -0600

    Add module generator

--- finance-generator/src/main/java/org/finance/generator/FinanceGenerator.java
+++ finance-generator/src/main/java/org/finance/generator/FinanceGenerator.java
@@ -1,6 +1,6 @@
 package org.finance.generator;
 
-import com.miu.finance.model.FinanceData;
+import com.miu.finance.model.FinanceDataHBase;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -11,15 +11,7 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-<<<<<<< Updated upstream
-import org.apache.spark.sql.AnalysisException;
-
-import com.miu.finance.model.FinanceData;
-import com.miu.finance.model.FinanceDataHBase;
-
-=======
 import org.finance.hbasereader.HBaseReader;
->>>>>>> Stashed changes
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -28,41 +20,11 @@ import java.util.List;
 public class FinanceGenerator {
 
     public static void main(String[] args) {
+        System.out.println("0000000000");
         SparkConf conf = new SparkConf().setAppName("FinanceDataAnalysis").setMaster("local[*]");
         JavaSparkContext sc = null;
         SparkSession spark = null;
 
-<<<<<<< Updated upstream
-		try {
-			sc = new JavaSparkContext(conf);
-			spark = SparkSession.builder()
-					.appName("FinanceDataAnalysis")
-					.config(conf)
-					.getOrCreate();
-			System.out.println("66666");
-			showFinanceDataAnalysis(sc, spark);
-		} catch (Exception e) {
-			e.printStackTrace(); // Log the exception
-		} finally {
-			if (spark != null) {
-				spark.stop();
-			}
-			if (sc != null) {
-				sc.close();
-			}
-		}
-	}
-	private static void showFinanceDataAnalysis(JavaSparkContext sc, SparkSession spark) throws IOException {
-		JavaRDD<FinanceDataHBase> financeDataRDD = sc.parallelize(new HBaseReader().GetFiannceAnalysis());
-		// Define the schema based on FinanceData.Price fields
-		String schemaString = "key date open high low close volume adjclose";
-		
-		List<StructField> fields = new ArrayList<>();
-		for (String fieldName : schemaString.split(" ")) {
-			StructField field = DataTypes.createStructField(fieldName, fieldName.equals("date") || fieldName.equals("volume") ? DataTypes.LongType : DataTypes.DoubleType, true);
-			fields.add(field);
-		}
-=======
         try {
             sc = new JavaSparkContext(conf);
             spark = SparkSession.builder()
@@ -71,7 +33,7 @@ public class FinanceGenerator {
                     .getOrCreate();
             showFinanceDataAnalysis(sc, spark);
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Log the exception
         } finally {
             if (spark != null) {
                 spark.stop();
@@ -83,50 +45,34 @@ public class FinanceGenerator {
     }
 
     private static void showFinanceDataAnalysis(JavaSparkContext sc, SparkSession spark) throws IOException {
-        JavaRDD<FinanceData.Price> financeDataRDD = sc.parallelize(new HBaseReader().getFinanceAnalysis());
-        String schemaString = "date open high low close volume adjclose";
+        JavaRDD<FinanceDataHBase> financeDataRDD = sc.parallelize(new HBaseReader().GetFiannceAnalysis());
+        // Define the schema based on FinanceData.Price fields
+        String schemaString = "key date open high low close volume adjclose";
 
         List<StructField> fields = new ArrayList<>();
         for (String fieldName : schemaString.split(" ")) {
             StructField field = DataTypes.createStructField(fieldName, fieldName.equals("date") || fieldName.equals("volume") ? DataTypes.LongType : DataTypes.DoubleType, true);
             fields.add(field);
         }
->>>>>>> Stashed changes
 
         StructType schema = DataTypes.createStructType(fields);
 
-<<<<<<< Updated upstream
-		JavaRDD<Row> rowRDD = financeDataRDD.map((FinanceDataHBase record) -> RowFactory.create(record.getKey(), record.getPrice().getDate(), record.getPrice().getOpen(), 
-																record.getPrice().getHigh(), record.getPrice().getLow(), record.getPrice().getClose(), record.getPrice().getVolume(), record.getPrice().getAdjclose()));
-=======
-        JavaRDD<Row> rowRDD = financeDataRDD.map((FinanceData.Price record) -> RowFactory.create(record.getDate(), record.getOpen(), record.getHigh(), record.getLow(), record.getClose(), record.getVolume(), record.getAdjclose()));
->>>>>>> Stashed changes
+        JavaRDD<Row> rowRDD = financeDataRDD.map((FinanceDataHBase record) -> RowFactory.create(record.getKey(), record.getPrice().getDate(), record.getPrice().getOpen(),
+                record.getPrice().getHigh(), record.getPrice().getLow(), record.getPrice().getClose(), record.getPrice().getVolume(), record.getPrice().getAdjclose()));
 
         Dataset<Row> financeDataFrame = spark.createDataFrame(rowRDD, schema);
         financeDataFrame.createOrReplaceTempView("financeData");
 
-<<<<<<< Updated upstream
-		// Example SQL queries on finance data
-		Dataset<Row> summaryResult = spark.sql("SELECT key, date, open, high, low, close, volume FROM financeData WHERE key != 'NULL'");
-		summaryResult.show(5);
+        // Example SQL queries on finance data
+        Dataset<Row> summaryResult = spark.sql("SELECT key, date, open, high, low, close, volume FROM financeData WHERE key != 'NULL'");
+        summaryResult.show(5);
 
 //		Dataset<Row> avgVolume = spark.sql("SELECT AVG(volume) as avg_volume FROM financeData");
 //		avgVolume.show(5);
 
-		// You can modify the file paths according to your HDFS setup or requirements
-		summaryResult.write().mode("append").option("header", "true").csv("hdfs://localhost/user/cloudera/FinanceDataSummary");
-//		avgVolume.write().mode("append").option("header", "true").csv("hdfs://localhost/user/cloudera/FinanceDataAvgVolume");
-	}
-=======
-        Dataset<Row> summaryResult = spark.sql("SELECT date, open, high, low, close, volume FROM financeData");
-        summaryResult.show();
-
-        Dataset<Row> avgVolume = spark.sql("SELECT AVG(volume) as avg_volume FROM financeData");
-        avgVolume.show();
-
+        // You can modify the file paths according to your HDFS setup or requirements
         summaryResult.write().mode("append").option("header", "true").csv("hdfs://localhost/user/cloudera/FinanceDataSummary");
-        avgVolume.write().mode("append").option("header", "true").csv("hdfs://localhost/user/cloudera/FinanceDataAvgVolume");
+//		avgVolume.write().mode("append").option("header", "true").csv("hdfs://localhost/user/cloudera/FinanceDataAvgVolume");
     }
->>>>>>> Stashed changes
 }
 
--- finance-generator/src/main/java/org/finance/hbasereader/HBaseReader.java
+++ finance-generator/src/main/java/org/finance/hbasereader/HBaseReader.java
@@ -1,6 +1,7 @@
 package org.finance.hbasereader;
 
 import com.miu.finance.model.FinanceData;
+import com.miu.finance.model.FinanceDataHBase;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
@@ -15,93 +16,48 @@ import java.util.List;
 
 import static com.miu.finance.utils.FinanceUtils.TABLE_NAME;
 
-<<<<<<< Updated upstream
-	public List<FinanceDataHBase> GetFiannceAnalysis() throws IOException
-	{
-//		List<FinanceData.Price> priceList=new ArrayList<FinanceData.Price>();
-		List<FinanceDataHBase> dataHBaseList=new ArrayList<FinanceDataHBase>();
-	
-		try (Connection connection = ConnectionFactory.createConnection(this.hbaseConfig))
-		{
-			Table table = connection.getTable(TableName.valueOf(TABLE_NAME));
-			Scan scan = new Scan();
-			scan.setCacheBlocks(false);
-			scan.setCaching(10000);
-			scan.setMaxVersions(10);
-=======
 public class HBaseReader {
-
-    private final Configuration hbaseConfig;
->>>>>>> Stashed changes
+    private Configuration hbaseConfig;
 
     public HBaseReader() {
         this.hbaseConfig = HBaseConfiguration.create();
     }
 
-<<<<<<< Updated upstream
-			for (Result result : scanner) {
-			//	FinanceDataHBase hBaseData = new FinanceDataHBase();
-				System.out.println("price: " + result);
-				FinanceDataHBase hBaseData = parseResultToPrice(result);
-				if (hBaseData != null) {
-					dataHBaseList.add(hBaseData);
-				}
-			}
-		}
-		return dataHBaseList;
-	}
-	private FinanceDataHBase parseResultToPrice(Result result) {
+    public List<FinanceDataHBase> GetFiannceAnalysis() throws IOException {
+        List<FinanceDataHBase> dataHBaseList = new ArrayList<>();
 
-		FinanceData.Price price = new FinanceData.Price();
-		FinanceDataHBase hBaseData = new FinanceDataHBase();
-		
-	    // Extracting the row key from the Result object
-	    String rowKey = Bytes.toString(result.getRow());
-	    hBaseData.setKey(rowKey); // Setting the row key on the Price object
-		for (Cell cell : result.listCells()) {
-			String qualifier = Bytes.toString(CellUtil.cloneQualifier(cell));
-			String value = Bytes.toString(CellUtil.cloneValue(cell));
+        try (Connection connection = ConnectionFactory.createConnection(this.hbaseConfig)) {
+            Table table = connection.getTable(TableName.valueOf(TABLE_NAME));
+            Scan scan = new Scan();
+            scan.setCacheBlocks(false);
+            scan.setCaching(10000);
+            scan.setMaxVersions(10);
 
-			switch (qualifier) {
+            ResultScanner scanner = table.getScanner(scan);
 
-			case "date":
-				price.setDate((long) Double.parseDouble(value));
-				break;
-			case "open":
-				price.setOpen(Double.parseDouble(value));
-				break;
-			case "high":
-				price.setHigh(Double.parseDouble(value));
-				break;
-			case "low":
-				price.setLow(Double.parseDouble(value));
-				break;
-			case "close":
-				price.setClose(Double.parseDouble(value));
-				break;
-			case "volume":
-				price.setVolume((long)Double.parseDouble(value));
-				break;
-			case "adjclose":
-				price.setAdjclose(Double.parseDouble(value));
-				break;
-			default:
-				// Handle any other columns you may have
-				break;
-			}
-		}
-		hBaseData.setPrice(price);
-		return hBaseData;
-	}
-=======
-    private static FinanceData.Price parseResultToPrice(Result result) {
+            for (Result result : scanner) {
+                //	FinanceDataHBase hBaseData = new FinanceDataHBase();
+                System.out.println("price: " + result);
+                FinanceDataHBase hBaseData = parseResultToPrice(result);
+                dataHBaseList.add(hBaseData);
+            }
+        }
+        return dataHBaseList;
+    }
+
+    private FinanceDataHBase parseResultToPrice(Result result) {
         FinanceData.Price price = new FinanceData.Price();
+        FinanceDataHBase hBaseData = new FinanceDataHBase();
+
+        // Extracting the row key from the Result object
+        String rowKey = Bytes.toString(result.getRow());
+        hBaseData.setKey(rowKey); // Setting the row key on the Price object
         for (Cell cell : result.listCells()) {
             String qualifier = Bytes.toString(CellUtil.cloneQualifier(cell));
             String value = Bytes.toString(CellUtil.cloneValue(cell));
             switch (qualifier) {
                 case "date":
-                    price.setDate(Long.parseLong(value));
+                    price.setDate((long) Double.parseDouble(value));
                     break;
                 case "open":
                     price.setOpen(Double.parseDouble(value));
@@ -116,37 +72,17 @@ public class HBaseReader {
                     price.setClose(Double.parseDouble(value));
                     break;
                 case "volume":
-                    price.setVolume(Long.parseLong(value));
+                    price.setVolume((long) Double.parseDouble(value));
                     break;
                 case "adjclose":
                     price.setAdjclose(Double.parseDouble(value));
                     break;
                 default:
+                    // Handle any other columns you may have
                     break;
             }
         }
-        return price;
+        hBaseData.setPrice(price);
+        return hBaseData;
     }
-
-    public List<FinanceData.Price> getFinanceAnalysis() throws IOException {
-        List<FinanceData.Price> priceList = new ArrayList<>();
-
-        try (Connection connection = ConnectionFactory.createConnection(this.hbaseConfig)) {
-            Table table = connection.getTable(TableName.valueOf(TABLE_NAME));
-            Scan scan = new Scan();
-            scan.setCacheBlocks(false);
-            scan.setCaching(10000);
-            scan.setMaxVersions(10);
-
-            ResultScanner scanner = table.getScanner(scan);
-
-            for (Result result : scanner) {
-                System.out.println("price: " + result);
-                FinanceData.Price price = parseResultToPrice(result);
-                priceList.add(price);
-            }
-        }
-        return priceList;
-    }
->>>>>>> Stashed changes
 }
